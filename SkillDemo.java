3aa45fd3273b15c4bb5f4a74fada259f80e44385
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2022-05-01 16:04:01 -0700

    added method and jnuit

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo{
+    public static boolean isEven(int num){
+        return false; //initially fail the test
+    }
+}
---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTester {
+    @Test 
+    public void testIsEven(){
+        assertTrue(SkillDemo.isEven(2));
+    }
+
+}
